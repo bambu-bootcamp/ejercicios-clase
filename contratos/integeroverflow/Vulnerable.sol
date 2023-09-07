9468e62438b3f36bce9ea62f035070241c31a73f
Mauricio Henao <atomauro@cosmos-2.local>
2023-09-06 19:33:54 -0500

    .

--- contratos/integeroverflow/Vulnerable.sol
+++ contratos/integeroverflow/Vulnerable.sol
@@ -5,6 +5,6 @@ contract IntegerOverflowVulnerableContract {
     uint8 public value = 255;
 
     function increment() public {
-        value += 1;
+        value += 1; //0,1,2,3,4,5,6,7,8,9,10
     }
 }
